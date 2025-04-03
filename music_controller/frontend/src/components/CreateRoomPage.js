af3455143221b192ea12f1b8748b99f499e0155a
Rohan Bhagi <rohanbhagi99@gmail.com>
2025-04-03 09:39:51 +0530

    Added UI for create room page, needs more work, incomplete

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
+import { Button, Grid } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 export default class CreateRoomPage extends Component {
+  deafult_votes = 2;
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <p>This is the create room page</p>;
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography component="h4" variant="h4">
+            Create A Room
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl component="fieldset">
+            <FormHelperText>
+              <div align="center">Guest can control playback.</div>
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+      </Grid>
+    );
   }
 }
